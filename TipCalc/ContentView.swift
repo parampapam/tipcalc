36ebd9d8ba48d1c2597e0d63aaccc41cd2859618
Roman Pospelov <romanpospelov@gmail.com>
2020-11-09 18:28:50 +0300

    Initial Commit

---
+++ TipCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TipCalc
+//
+//  Created by Роман Поспелов on 09.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TipCalc/TipCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalcApp.swift
+//  TipCalc
+//
+//  Created by Роман Поспелов on 09.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct TipCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
